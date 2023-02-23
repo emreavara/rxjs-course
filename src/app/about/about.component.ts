f9afc43e93aa0707ae69257121a4bc2200c5b948
Emre Avara <emre_avara@hotmail.com>
2023-02-23 15:05:45 +0100

    Add subscribe and unsubscribe example with interval from rxjs

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,30 +1,55 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { interval, timer, fromEvent } from "rxjs";
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
+  constructor() {}
+  // Without RxJs
 
-  constructor() { }
-
+  /*
   ngOnInit() {
-    // Continuous Streams
+    // Nested callbacks will be triggered on each click event
+    // Try to click and see multiple streams
     document.addEventListener('click', event => {
       console.log(event)
+      setTimeout(()=>{
+        console.log("finished...")
+        // Continuous Streams
+        let counter:number = 0;
+        setInterval(()=>{
+          console.log(counter)
+          counter++;
+        }, 1000)
+      }, 3000)
+
     })
+  }*/
 
-    let counter:number = 0;
+  // With RxJs
 
-    // setInterval(()=>{
-    //   console.log(counter)
-    //   counter++;
-    // }, 1000)
+  ngOnInit(): void {
+    // Simple interval observable example
 
-    setTimeout(()=>{
-      console.log("finished...")
-    }, 3000)
+    // const interval$ = interval(1000);
+    // interval$.subscribe(val => console.log("Stream 1: ",val))
 
+    // interval$.subscribe(val => console.log("Stream 2: ",val))
+
+    // Subscribe & Unsubscribe example
+
+    // const interval$ = timer(3000, 1000);
+    // const sub = interval$.subscribe((val) => console.log("Stream 1: ", val));
+
+    // setTimeout(()=> sub.unsubscribe(), 5000)
+
+    // const click$ = fromEvent(document, "click");
+    // click$.subscribe(
+    //   (event) => console.log(event),
+    //   (err) => console.log(err),
+    //   ()=> console.log("completed...")
+    // );
   }
 }
