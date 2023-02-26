5f2efe577ce246c8860a0f426cfc78511c7c227e
Emre Avara <emre_avara@hotmail.com>
2023-02-27 00:13:30 +0100

    Add save course

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -11,6 +11,8 @@ import {
   merge,
   Subject,
   BehaviorSubject,
+  AsyncSubject,
+  ReplaySubject,
 } from "rxjs";
 import { delayWhen, filter, map, take, timeout } from "rxjs/operators";
 import { createHttpObservable } from "../common/util";
@@ -22,16 +24,43 @@ import { createHttpObservable } from "../common/util";
 })
 export class AboutComponent implements OnInit {
   ngOnInit() {
-    const subject = new BehaviorSubject(0);
+    // const subject = new BehaviorSubject(0);
+    // const series$ = subject.asObservable();
+    // series$.subscribe((val) => console.log("early sub: ", val));
+    // subject.next(1);
+    // subject.next(2);
+    // subject.next(3);
+    // // subject.complete();
+
+    // setTimeout(() => {
+    //   series$.subscribe((val) => console.log("late sub: ", val));
+    //   subject.next(4);
+    // }, 3000);
+
+    // const subject = new AsyncSubject();
+    // const series$ = subject.asObservable();
+    // series$.subscribe((val) => console.log("early sub: ", val));
+    // subject.next(1);
+    // subject.next(2);
+    // subject.next(3);
+    // // Without completion it won't return any value.
+    // subject.complete();
+
+    // setTimeout(() => {
+    //   series$.subscribe((val) => console.log("second sub: ", val));
+    // }, 3000);
+
+    const subject = new ReplaySubject();
     const series$ = subject.asObservable();
     series$.subscribe((val) => console.log("early sub: ", val));
     subject.next(1);
     subject.next(2);
     subject.next(3);
+    // Without completion it won't return any value.
     // subject.complete();
 
     setTimeout(() => {
-      series$.subscribe((val) => console.log("late sub: ", val));
+      series$.subscribe((val) => console.log("second sub: ", val));
       subject.next(4);
     }, 3000);
   }
